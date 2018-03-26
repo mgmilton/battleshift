d5396673fd74b8a79b67a074c1d9648b85f31da1
mgmilton <matt.milton@gmail.com>
2018-03-26 16:35:33 -0600

    adds update with password on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :email, :name, :password
+  validates_presence_of :email, :name
+  validates :password, presence: true, on: :creates
+  validates :password, presence: true, on: :update, allow_blank: true
   validates_confirmation_of :password
   enum status: %w(inactivated activated)
 
