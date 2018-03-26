fae959c162d278a66870f1c1322d36f9dbeafea2
mgmilton <matt.milton@gmail.com>
2018-03-26 17:46:38 -0600

    adds authentication for api endpoints

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,15 @@
 class ApiController < ActionController::API
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    api_key = request.headers['X-API-Key']
+    @current_user = User.find_by(api_key: api_key) if api_key
+
+    unless @current_user
+      render status: :unauthorized
+      return false
+    end
+  end
 end
