e278ae0e0efa769e91c6a75697af6e09a0f416d0
mgmilton <matt.milton@gmail.com>
2018-03-26 11:23:54 -0600

    adds user validation test

---
+++ app/controllers/register_controller.rb
@@ -0,0 +1,5 @@
+class RegisterController < ApplicationController
+  def create
+    
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/factory_bot'
+
 
 SimpleCov.start "rails"
 
@@ -24,7 +24,7 @@ SimpleCov.start "rails"
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
