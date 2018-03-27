5bc7020f6eff85dde91624e8a122618a25140390
mgmilton <matt.milton@gmail.com>
2018-03-27 13:34:28 -0600

    refactors game.save in ships controller

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -4,11 +4,11 @@ class Api::V1::Games::ShipsController < ApiController
     if game.game_users.find_by(user: @current_user).player_1?
       ShipPlacer.new(board: game.player_1_board, ship: Ship.new(params[:ship_size]), start_space: params[:start_space], end_space: params[:end_space]).run
       ships_placed =  game.player_1_board.board.flatten.map(&:values).flatten.map(&:contents).uniq.compact.length
-      game.save!
     elsif game.game_users.find_by(user: @current_user).player_2?
         ShipPlacer.new(board: game.player_2_board, ship: Ship.new(params[:ship_size]), start_space: params[:start_space], end_space: params[:end_space]).run
         ships_placed = game.player_2_board.board.flatten.map(&:values).flatten.map(&:contents).uniq.compact.length
     end
+    game.save!
     render json: game, message: ShipPlacer.message_formatter(params[:ship_size], ships_placed)
   end
 end
