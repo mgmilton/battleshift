c1326692af2bc74ad1082d0d6ddfb4f17b52b15a
mgmilton <matt.milton@gmail.com>
2018-04-01 12:59:05 -0600

    Adds vcr mocking to ships and shots spec

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ end
 
 group :test do
   gem 'simplecov'
+  gem 'vcr'
+  gem 'webmock'
 end
 
 group :development do
--- spec/requests/api/v1/games/shots_spec.rb
+++ spec/requests/api/v1/games/shots_spec.rb
@@ -11,151 +11,162 @@ describe "Api::V1::Shots" do
     let(:user) { create(:user)}
     let(:opponent) { create(:opponent) }
     it "updates the message and board with a hit" do
-      ShipPlacer.new(board: player_2_board,
-                     ship: sm_ship,
-                     start_space: "A1",
-                     end_space: "A2").run
+      VCR.use_cassette("Shots", :record => :new_episodes) do
+        ShipPlacer.new(board: player_2_board,
+                       ship: sm_ship,
+                       start_space: "A1",
+                       end_space: "A2").run
 
-      create(:game_user, game_id: initial_game.id, user_id: opponent.id)
-      create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
-      headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
-      json_payload = {target: "A1"}.to_json
-      post "/api/v1/games/#{initial_game.id}/shots", headers: headers, params: json_payload
+        create(:game_user, game_id: initial_game.id, user_id: opponent.id)
+        create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
+        headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
+        json_payload = {target: "A1"}.to_json
+        post "/api/v1/games/#{initial_game.id}/shots", headers: headers, params: json_payload
 
-      expect(response).to be_success
+        expect(response).to be_success
 
-      game = JSON.parse(response.body, symbolize_names: true)
-      expected_messages = "Your shot resulted in a Hit."
-      player_2_targeted_space = game[:player_2_board][:rows].first[:data].first[:status]
-      expect(game[:message]).to eq expected_messages
-      expect(player_2_targeted_space).to eq("Hit")
+        game = JSON.parse(response.body, symbolize_names: true)
+        expected_messages = "Your shot resulted in a Hit."
+        player_2_targeted_space = game[:player_2_board][:rows].first[:data].first[:status]
+        expect(game[:message]).to eq expected_messages
+        expect(player_2_targeted_space).to eq("Hit")
+      end
     end
 
     it 'calls the turn processor' do
-      ShipPlacer.new(board: player_2_board,
-                     ship: sm_ship,
-                     start_space: "A1",
-                     end_space: "A2").run
+      VCR.use_cassette("Shots", :record => :new_episodes) do
+        ShipPlacer.new(board: player_2_board,
+                       ship: sm_ship,
+                       start_space: "A1",
+                       end_space: "A2").run
 
-      create(:game_user, game_id: initial_game.id, user_id: opponent.id)
-      create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
-      headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
-      json_payload = {target: "A1"}.to_json
+        create(:game_user, game_id: initial_game.id, user_id: opponent.id)
+        create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
+        headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
+        json_payload = {target: "A1"}.to_json
 
-      allow(TurnProcessor).to receive(:new).and_call_original
+        allow(TurnProcessor).to receive(:new).and_call_original
 
-      post "/api/v1/games/#{initial_game.id}/shots", headers: headers, params: json_payload
+        post "/api/v1/games/#{initial_game.id}/shots", headers: headers, params: json_payload
 
-      expect(TurnProcessor).to have_received(:new).with(
-        an_instance_of(Game), "A1", anything, "challenger"
-      )
+        expect(TurnProcessor).to have_received(:new).with(
+          an_instance_of(Game), "A1", anything, "challenger"
+        )
+      end
     end
 
     it "updates the message and board with a miss" do
-      ShipPlacer.new(board: player_2_board,
-                     ship: sm_ship,
-                     start_space: "A1",
-                     end_space: "A2").run
+      VCR.use_cassette("Shots", :record => :new_episodes) do
+        ShipPlacer.new(board: player_2_board,
+                       ship: sm_ship,
+                       start_space: "A1",
+                       end_space: "A2").run
 
-      create(:game_user, game_id: initial_game.id, user_id: opponent.id)
-      create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
-      headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
+        create(:game_user, game_id: initial_game.id, user_id: opponent.id)
+        create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
+        headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
 
-      json_payload = {target: "D1"}.to_json
-      post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
+        json_payload = {target: "D1"}.to_json
+        post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
 
-      expect(response).to be_success
+        expect(response).to be_success
 
-      game = JSON.parse(response.body, symbolize_names: true)
+        game = JSON.parse(response.body, symbolize_names: true)
 
-      expected_messages = "Your shot resulted in a Miss."
-      player_2_targeted_space = game[:player_2_board][:rows].last[:data].first[:status]
+        expected_messages = "Your shot resulted in a Miss."
+        player_2_targeted_space = game[:player_2_board][:rows].last[:data].first[:status]
 
 
-      expect(game[:message]).to eq expected_messages
-      expect(player_2_targeted_space).to eq("Miss")
+        expect(game[:message]).to eq expected_messages
+        expect(player_2_targeted_space).to eq("Miss")
+      end
     end
 
-
     it "updates the message and board with a sunk battleship" do
-      ShipPlacer.new(board: player_2_board,
-                     ship: sm_ship,
-                     start_space: "A1",
-                     end_space: "A2").run
-      create(:game_user, game_id: initial_game.id, user_id: opponent.id)
-      create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
+      VCR.use_cassette("Shots", :record => :new_episodes) do
+        ShipPlacer.new(board: player_2_board,
+                       ship: sm_ship,
+                       start_space: "A1",
+                       end_space: "A2").run
+        create(:game_user, game_id: initial_game.id, user_id: opponent.id)
+        create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
 
-      headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
-      json_payload = {target: "A1"}.to_json
-      post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
-      initial_game.reload
+        headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
+        json_payload = {target: "A1"}.to_json
+        post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
+        initial_game.reload
 
-      #opponent shoots
-      headers = {"X-Api-Key" => opponent.api_key, "CONTENT_TYPE" => "application/json" }
-      json_payload = {target: "D1"}.to_json
-      post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
-      initial_game.reload
-      initial_game.update_attributes(winner: nil)
+        #opponent shoots
+        headers = {"X-Api-Key" => opponent.api_key, "CONTENT_TYPE" => "application/json" }
+        json_payload = {target: "D1"}.to_json
+        post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
+        initial_game.reload
+        initial_game.update_attributes(winner: nil)
 
-      headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
-      json_payload = {target: "A2"}.to_json
-      post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
-
-
-      expect(response).to be_success
-
-      game = JSON.parse(response.body, symbolize_names: true)
-
-      expected_messages = "Your shot resulted in a Hit. Battleship sunk. Game over."
-      player_2_targeted_space = game[:player_2_board][:rows].first[:data].first[:status]
+        headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
+        json_payload = {target: "A2"}.to_json
+        post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
 
 
-      expect(game[:message]).to eq expected_messages
-      expect(player_2_targeted_space).to eq("Hit")
-      expect(game[:winner]).to eq(user.email)
+        expect(response).to be_success
 
-      # user cannot shoot after game is over
-      headers = {"X-Api-Key" => opponent.api_key, "CONTENT_TYPE" => "application/json" }
-      json_payload = {target: "A1"}.to_json
-      post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
+        game = JSON.parse(response.body, symbolize_names: true)
 
-      game = JSON.parse(response.body, symbolize_names: true)
-      expect(response.status).to eq(400)
-      expect(game[:message]).to include("Invalid move. Game over.")
+        expected_messages = "Your shot resulted in a Hit. Battleship sunk. Game over."
+        player_2_targeted_space = game[:player_2_board][:rows].first[:data].first[:status]
+
+
+        expect(game[:message]).to eq expected_messages
+        expect(player_2_targeted_space).to eq("Hit")
+        expect(game[:winner]).to eq(user.email)
+
+        # user cannot shoot after game is over
+        headers = {"X-Api-Key" => opponent.api_key, "CONTENT_TYPE" => "application/json" }
+        json_payload = {target: "A1"}.to_json
+        post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
+
+        game = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(400)
+        expect(game[:message]).to include("Invalid move. Game over.")
+      end
     end
 
     it "updates the message but not the board with invalid coordinates" do
-      player_1_board = Board.new(1)
-      player_2_board = Board.new(1)
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
-      create(:game_user, game_id: game.id, user_id: opponent.id)
-      create(:game_user, game_id: game.id, user_id: user.id, player: 0)
+      VCR.use_cassette("Shots", :record => :new_episodes) do
+        player_1_board = Board.new(1)
+        player_2_board = Board.new(1)
+        game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+        create(:game_user, game_id: game.id, user_id: opponent.id)
+        create(:game_user, game_id: game.id, user_id: user.id, player: 0)
 
-      headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
-      json_payload = {target: ""}.to_json
-      post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: headers
-      game = JSON.parse(response.body, symbolize_names: true)
-      expect(game[:message]).to eq "Invalid coordinates."
+        headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
+        json_payload = {target: ""}.to_json
+        post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: headers
+        game = JSON.parse(response.body, symbolize_names: true)
+        expect(game[:message]).to eq "Invalid coordinates."
+      end
     end
 
     it "updates the message but not the board two consecutive shots from a user" do
-      ShipPlacer.new(board: player_2_board,
-                     ship: sm_ship,
-                     start_space: "A1",
-                     end_space: "A2").run
-      create(:game_user, game_id: initial_game.id, user_id: opponent.id)
-      create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
+      VCR.use_cassette("Shots", :record => :new_episodes) do
+        ShipPlacer.new(board: player_2_board,
+                       ship: sm_ship,
+                       start_space: "A1",
+                       end_space: "A2").run
+        create(:game_user, game_id: initial_game.id, user_id: opponent.id)
+        create(:game_user, game_id: initial_game.id, user_id: user.id, player: 0)
 
-      headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
-      json_payload = {target: "A1"}.to_json
-      post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
-      initial_game.reload
+        headers = {"X-Api-Key" => user.api_key, "CONTENT_TYPE" => "application/json" }
+        json_payload = {target: "A1"}.to_json
+        post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
+        initial_game.reload
 
-      #test user cannot shoot twice in a row
-      post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
-      initial_game.reload
-      game = JSON.parse(response.body, symbolize_names: true)
-      expect(game[:message]).to eq "Invalid move. It's your opponent's turn"
+        #test user cannot shoot twice in a row
+        post "/api/v1/games/#{initial_game.id}/shots", params: json_payload, headers: headers
+        initial_game.reload
+        game = JSON.parse(response.body, symbolize_names: true)
+        expect(game[:message]).to eq "Invalid move. It's your opponent's turn"
+      end
     end
   end
 end
