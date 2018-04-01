6f3d127877cb85af0a1032df6339760d1627da46
mgmilton <matt.milton@gmail.com>
2018-04-01 17:49:21 -0600

    updates travis.yml

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'twilio-ruby'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'coveralls', require: false
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'factory_bot_rails'
   gem 'rspec-rails'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,8 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'byebug'
 require 'twilio_mock'
-
-
+require 'coveralls'
+Coveralls.wear!
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
